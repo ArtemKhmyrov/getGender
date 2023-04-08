90bdec814b95659a2b8871725c159bab5c6a4578
hmyrovam <a.hmyrov-saratov@mail.ru>
2023-04-08 21:19:32 +0400

    refactor: get gender only last name

--- src/php/gender.php
+++ src/php/gender.php
@@ -10,23 +10,7 @@ if(!empty($_POST['lastname'])){
         else if(substr($lastName, -4) == 'на' || substr($lastName, -4) == 'зы'){
             echo 'Женское отчество';
         }
-        else {
-            if(!empty($_POST['name'])){
-                $name = $_POST['name'];
-                if(preg_match('/[^а-я ]/',$name)){
-                    if(!substr($name, -2) == 'а'){
-                        echo 'Мужское имя';
-                    }
-                    else if(substr($name, -2) == 'а' || !substr($name, -2) == 'я' || !substr($name, -2) == 'э' || !substr($name, -2) == 'и'){
-                        echo 'Женское имя';
-                    }
-                }
-                else {
-                    echo 'Отчество может состоять только из кириллицы!';
-                    
-                }
-            }
-        }   
+       
     }
     
     else {
@@ -35,30 +19,7 @@ if(!empty($_POST['lastname'])){
 
 }
 else{
-    $name = $_POST['name'];
-    
-    if(!empty($_POST['name'])){
-        $name = $_POST['name'];
-        $kek = substr($name, -2);
-        
-        if(preg_match('/[^а-я ]/',$name)){
-         
-            
-            if(substr($name, -2) == 'а' || substr($name, -2) == 'я' || substr($name, -2) == 'э' || substr($name, -2) == 'и'){
-                echo 'Женское имя';
-            }
-            else {
-                echo 'Мужское имя';
-            }
-        }
-        else {
-            echo 'Отчество может состоять только из кириллицы!';
-        }
-    }
-    else {
-
-        echo 'Пол определить не удалось';
-    }
+    echo 'Пол определить не удалось';
 }
 
 
